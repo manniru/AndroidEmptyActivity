b0fd50901a6771901a8b26b6f55d4f58302c9e04
Mannir <manniru@gmail.com>
2019-05-21 01:02:42 +0100

    Add Firebase Listner

--- app/src/main/java/com/mannir/emptyactivity/RecyclerViewActivity.kt
+++ app/src/main/java/com/mannir/emptyactivity/RecyclerViewActivity.kt
@@ -7,20 +7,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import java.util.ArrayList
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.analytics.FirebaseAnalytics
-import android.R.id
-
-
-
-
-
-
-
 
 
 class RecyclerViewActivity : AppCompatActivity() {
@@ -32,6 +23,10 @@ class RecyclerViewActivity : AppCompatActivity() {
     // mannir
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
+    // Firebase
+    val database = FirebaseDatabase.getInstance()
+    val myRef = database.getReference("message")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
@@ -39,12 +34,13 @@ class RecyclerViewActivity : AppCompatActivity() {
 
         // Set Layout Manager
         linearLayoutManager = LinearLayoutManager(this)
-        recyclerView!!.layoutManager = linearLayoutManager
+        recyclerView!!.layoutManager = linearLayoutManager as RecyclerView.LayoutManager?
 
         // Limiting the size
         recyclerView!!.setHasFixedSize(true)
 
         // mannir
+
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         val bundle = Bundle()
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id")
@@ -55,18 +51,19 @@ class RecyclerViewActivity : AppCompatActivity() {
         // Initialize list items
         init()
 
-        // Firebase
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hello World form Android Studio")
-
+        // mannir
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 val value = dataSnapshot.getValue(String::class.java)
                 Log.d("13131", "Value is: " + value!!)
+
+                init2(value)
+
+//                callsList!!.add(Call(value, "10:45 PM"))
+
+
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -74,6 +71,12 @@ class RecyclerViewActivity : AppCompatActivity() {
                 Log.w("13131", "Failed to read value.", error.toException())
             }
         })
+
+
+
+//        myRef.setValue("Hello World form Android Studio")
+
+
 
     }
 
@@ -89,18 +92,50 @@ class RecyclerViewActivity : AppCompatActivity() {
         callsList!!.add(Call("John", "9:30 AM"))
         callsList!!.add(Call("Rob", "9:40 AM"))
         callsList!!.add(Call("Peter", "9:45 AM"))
-        callsList!!.add(Call("Jack", "9:50 AM"))
-        callsList!!.add(Call("Bob", "9:55 AM"))
-        callsList!!.add(Call("Sandy", "10:00 AM"))
-        callsList!!.add(Call("Kate", "10:05 AM"))
-        callsList!!.add(Call("Daniel", "10:10 AM"))
-        callsList!!.add(Call("Roger", "10:15 AM"))
-        callsList!!.add(Call("Sid", "10:20 AM"))
-        callsList!!.add(Call("Kora", "10:25 AM"))
-        callsList!!.add(Call("Nick", "10:30 AM"))
-        callsList!!.add(Call("Rose", "10:35 AM"))
-        callsList!!.add(Call("Mia", "10:40 AM"))
-        callsList!!.add(Call("Scott", "10:45 AM"))
+//        callsList!!.add(Call("Jack", "9:50 AM"))
+//        callsList!!.add(Call("Bob", "9:55 AM"))
+//        callsList!!.add(Call("Sandy", "10:00 AM"))
+//        callsList!!.add(Call("Kate", "10:05 AM"))
+//        callsList!!.add(Call("Daniel", "10:10 AM"))
+//        callsList!!.add(Call("Roger", "10:15 AM"))
+//        callsList!!.add(Call("Sid", "10:20 AM"))
+//        callsList!!.add(Call("Kora", "10:25 AM"))
+//        callsList!!.add(Call("Nick", "10:30 AM"))
+//        callsList!!.add(Call("Rose", "10:35 AM"))
+//        callsList!!.add(Call("Mia", "10:40 AM"))
+//        callsList!!.add(Call("Scott", "10:45 AM"))
+
+
+
+        // Set items to adapter
+        callsAdapter!!.setCallsFeed(callsList!!)
+        callsAdapter!!.notifyDataSetChanged()
+    }
+
+    private fun init2(value: String) {
+        callsList = ArrayList()
+
+        // Initiating Adapter
+        callsAdapter = CallsAdapter(this@RecyclerViewActivity)
+        recyclerView!!.adapter = callsAdapter
+
+        // Adding some demo data(Call Objects).
+        // You can get them from your data server
+
+        callsList!!.add(Call(value, "1:50 AM"))
+//        callsList!!.add(Call("Bob", "9:55 AM"))
+//        callsList!!.add(Call("Sandy", "10:00 AM"))
+//        callsList!!.add(Call("Kate", "10:05 AM"))
+//        callsList!!.add(Call("Daniel", "10:10 AM"))
+//        callsList!!.add(Call("Roger", "10:15 AM"))
+//        callsList!!.add(Call("Sid", "10:20 AM"))
+//        callsList!!.add(Call("Kora", "10:25 AM"))
+//        callsList!!.add(Call("Nick", "10:30 AM"))
+//        callsList!!.add(Call("Rose", "10:35 AM"))
+//        callsList!!.add(Call("Mia", "10:40 AM"))
+//        callsList!!.add(Call("Scott", "10:45 AM"))
+
+
 
         // Set items to adapter
         callsAdapter!!.setCallsFeed(callsList!!)
